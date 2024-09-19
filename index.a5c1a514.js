48e2b5f82e3c9598789b1ac7f02d68a0622e18fc
Elvira-Miasnykova <98357455+Elvira-Miasnykova@users.noreply.github.com>
2024-09-19 19:42:31 +0000

    Deploying to gh-pages from @ Elvira-Miasnykova/hotel-app@2ac73b7b3af167af0b7def836ab22dc3fed7324c 🚀

--- index.a5c1a514.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),console.log(n),e.setAttribute("aria-expanded",!n),console.log(n),t.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.a5c1a514.js.map
